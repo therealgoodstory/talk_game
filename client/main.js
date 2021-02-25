f18fda7a2ae91a96dff3e00b727ef0a574493a1f
Epimetheus <therealgoodstory@gmail.com>
2021-02-25 15:23:51 +0300

    initial commit

---
+++ client/main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Home = () => {
+    return (
+        <div>
+            123
+        </div>
+    )
+}
+
+const target = document.getElementById('root')
+
+ReactDOM.render(<Home/> , target)
