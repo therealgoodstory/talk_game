5d75d7df45e1989b5c61fae7348a0f3150e4dcbe
Epimetheus <therealgoodstory@gmail.com>
2021-02-25 15:49:28 +0300

    add webpack config

--- client/main.js
+++ client/main.js
@@ -1,14 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Home = () => {
-    return (
-        <div>
-            123
-        </div>
-    )
-}
+  return <div>123</div>;
+};
+const target = document.getElementById("root");
 
-const target = document.getElementById('root')
-
-ReactDOM.render(<Home/> , target)
+ReactDOM.render(<Home />, target);
